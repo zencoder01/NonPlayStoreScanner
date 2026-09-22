496ab2bd7d0007a4e3f2596fc68a43fea34ada09
Ignitious Samasumo Pesuka <43833569+zencoder01@users.noreply.github.com>
2026-09-22 07:03:40 +0200

    Add google() and mavenCentral() repositories for Android Gradle Plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,6 +25,11 @@ android {
     }
 }
 
+repositories {
+    google()
+    mavenCentral()
+}
+
 dependencies {
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.7.0")
--- build.gradle.kts
+++ build.gradle.kts
@@ -3,3 +3,9 @@ plugins {
     id("com.android.application") version "8.5.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.20" apply false
 }
+
+repositories {
+    google()
+    mavenCentral()
+    gradlePluginPortal()
+}
